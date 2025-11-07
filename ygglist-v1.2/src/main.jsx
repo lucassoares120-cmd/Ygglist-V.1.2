9699425018e00a4933b53468d8a227f2f6d81824
lucassoares120-cmd <lucas_soares120@hotmail.com>
2025-11-07 09:42:23 -0300

    Update main.jsx

--- ygglist-v1.2/src/main.jsx
+++ ygglist-v1.2/src/main.jsx
@@ -3,13 +3,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './modules/App.jsx'
 import './styles/index.css'
-+ import { startFaviconBreath } from './utils/faviconBreath.js'
+import { startFaviconBreath } from './utils/faviconBreath.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App/>
+    <App />
   </React.StrictMode>
 )
 
-+ // Inicia a “respiração” do favicon (suave)
-+ startFaviconBreath('/YggSymbol.png', 2400)
+// Inicia a “respiração” do favicon (suave)
+startFaviconBreath('/YggSymbol.png', 2400)
+
+// Registra o Service Worker para PWA instalável
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').catch(() => {});
+}
