e6c1bfcc331d637153dd0db6a2f5e6dfe4a271eb
lucassoares120-cmd <lucas_soares120@hotmail.com>
2025-11-07 09:30:32 -0300

    Update main.jsx

--- ygglist-v1.2/src/main.jsx
+++ ygglist-v1.2/src/main.jsx
@@ -3,4 +3,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './modules/App.jsx'
 import './styles/index.css'
-ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App/></React.StrictMode>)
++ import { startFaviconBreath } from './utils/faviconBreath.js'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>
+)
+
++ // Inicia a “respiração” do favicon (suave)
++ startFaviconBreath('/YggSymbol.png', 2400)
